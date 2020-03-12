a742e8671f9bf124ea36a182e8eee15ad666ef36
Stanislav Shchukin <schukinstan@gmail.com>
2020-03-12 22:25:17 +0300

    add babel loader

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+'use strict';
+const DEV_PATH = './source/';
+const PUBLIC_PATH = './dist/';
+const ROOT_PATH = './';
+
+const
+    gulp = require('gulp'),
+    //browserSync = require('browser-sync').create(),
+    //uglify = require('gulp-uglify-es').default,
+    //sourcemaps = require('gulp-sourcemaps'),
+    //plumber = require('gulp-plumber'), // для отслеживания ошибок
+    babel = require('gulp-babel');
+
+
+// transform js
+function jsTransform(fileSrcPath, fileDestPath) {
+    return () => {
+        return gulp.src(fileSrcPath)
+            //.pipe(plumber())
+            //.pipe(sourcemaps.init())
+            .pipe(babel()) // see .babelrc
+            //.pipe(uglify()) // minification
+            //.pipe(sourcemaps.write('.'))
+            .pipe(gulp.dest(fileDestPath))
+            //.pipe(browserSync.stream());
+    }
+}
+
+gulp.task('js', jsTransform(DEV_PATH + '*.js', PUBLIC_PATH));
+
+gulp.task('build', gulp.series('js'));
+
+function watch() {
+    browserSync.init({
+        proxy: PUBLIC_PATH,
+        // server: {baseDir: './'}
+    });
+    gulp.watch(DEV_PATH + '*.js', 'js');
+    gulp.watch(DEV_PATH + '*.js').on('change', browserSync.reload);
+    //gulp.watch(PUBLIC_PATH + '*.html').on('change', browserSync.reload);
+}
+
+exports.watch = watch;
